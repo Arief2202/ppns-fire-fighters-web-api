160fceddb0170951cfc6c1093365eec6fe5ba6c1
Arief <arief.d2202@gmail.com>
2024-03-10 22:28:16 +0700

    Revisi API Hydrant

--- api_hydrant.php
+++ api_hydrant.php
@@ -6,16 +6,20 @@
     if(isset($_GET['create']) || isset($_POST['create'])){
         $nomor;
         $lokasi;
+        $jenis;
         if(isset($_GET['create'])){
             $nomor = $_GET['nomor'];
             $lokasi = $_GET['lokasi'];
+            $jenis = $_GET['jenis'];
         }
         else if(isset($_POST['create'])){
             $nomor = $_POST['nomor'];
             $lokasi = $_POST['lokasi'];
+            $jenis = $_POST['jenis'];
         }
+        if($jenis != "ihb" || $jenis != "ohb") $jenis = "ihb";
         //2024-03-31 00:00:00 
-        $sql = "INSERT INTO `hydrant` (`id`, `nomor`, `lokasi`, `timestamp`) VALUES (NULL, '".$nomor."', '".$lokasi."', current_timestamp());";
+        $sql = "INSERT INTO `hydrant` (`id`, `nomor`, `lokasi`, `jenis_hydrant`, `timestamp`) VALUES (NULL, '".$nomor."', '".$lokasi."', '".$jenis."', current_timestamp());";
         $result = mysqli_query($conn, $sql);
         if($result){
             http_response_code(200);
@@ -23,13 +27,13 @@
             echo json_encode([
                 "status" => "success",
                 "data" => $data,
-                "pesan" => "Data Apar Berhasil Ditambahkan",
+                "pesan" => "Data Hydrant Berhasil Ditambahkan",
             ]);
         }
         else{
             echo json_encode([
                 "status" => "failed",
-                "pesan" => "Data Apar Gagal Ditambahkan",
+                "pesan" => "Data Hydrant Gagal Ditambahkan",
             ]);
         }
     }
@@ -51,23 +55,29 @@
         $id = null;
         $nomor = null;
         $lokasi = null;
+        $jenis = null;
         if(isset($_GET['update'])){
             if(isset($_GET['id'])) $id = $_GET['id'];
             if(isset($_GET['nomor'])) $nomor = $_GET['nomor'];
             if(isset($_GET['lokasi'])) $lokasi = $_GET['lokasi'];
+            if(isset($_GET['jenis'])) $jenis = $_GET['jenis'];
         }
         else if(isset($_POST['update'])){
             if(isset($_POST['id'])) $id = $_POST['id'];
             if(isset($_POST['nomor'])) $nomor = $_POST['nomor'];
             if(isset($_POST['lokasi'])) $lokasi = $_POST['lokasi'];
+            if(isset($_POST['jenis'])) $jenis = $_POST['jenis'];
         }
+        if($jenis != "ihb" && $jenis != "ohb" && $jenis != null) $jenis = "ihb";
         $data = mysqli_fetch_object(mysqli_query($conn, "SELECT * FROM `hydrant` WHERE id = ".$id));
         if($data){
             if($nomor != null) $data->nomor = $nomor;
             if($lokasi != null) $data->lokasi = $lokasi;
+            if($lokasi != null) $data->lokasi = $lokasi;
+            if($jenis != null) $data->jenis_hydrant = $jenis;
             
             //2024-03-31 00:00:00 
-            $sql = "UPDATE `hydrant` SET `nomor` = '".$data->nomor."', `lokasi` = '".$data->lokasi."' WHERE `hydrant`.`id` = ".$id.";";
+            $sql = "UPDATE `hydrant` SET `nomor` = '".$data->nomor."', `lokasi` = '".$data->lokasi."', `jenis_hydrant` = '".$data->jenis_hydrant."' WHERE `hydrant`.`id` = ".$id.";";
             $result = mysqli_query($conn, $sql);
             if($result){
                 http_response_code(200);
